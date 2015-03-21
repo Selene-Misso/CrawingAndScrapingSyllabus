fbd1419b3c8c24b3754edad1287753b3fd9dd358
Selene-Misso <selene.jaxa@gmail.com>
2015-03-21 10:45:53 +0900

    取得した値を配列に保存するようにした

--- crawer.rb
+++ crawer.rb
@@ -7,56 +7,49 @@ opts = {
 
 url = "http://syllabus.jimu.kumamoto-u.ac.jp/kusy_detail.php?nendo=2014&scd=58&jcd=07092"
 
+attributes = ["授業科目名(日本語)","授業科目名(英語)","時間割コード","開講年次", 
+              "学期","曜日・時限","科目コード","科目分類", "選択／必修", "単位数",
+              "講義題目","担当教員","教科書","担当教員氏名","担当教員所属"]
+values     = []
+
 Anemone.crawl(url, opts) do |anemone|
 	anemone.on_every_page do |page|
+
 		# 授業科目名
-		puts "-----\n"
-		attrib = ["授業科目名(日本語)","授業科目名(英語)"]
-		i = 0
 		page.doc.xpath("/html/body//table[@class='detail']//td[@class='kamokumei']").each do |node|
-			title = node.xpath("./text()").to_s
-			puts attrib[i].to_s + "\t: " + title
-			i += 1
+			values.push(node.xpath("./text()").to_s)
 		end		
 		
 		# 時間割コード, 開講年次, 学期, 曜日・時限, 科目コード, 
 		# 科目分類,選択／必修, 単位数
-		puts "-----\n"
-		attrib = ["時間割コード", "開講年次", "学期", "曜日・時限", 
-				"科目コード","科目分類", "選択／必修", "単位数"]
-		i = 0
 		page.doc.xpath("/html/body//table[@class='detail']//td[@class='basic1']").each do |node|
-			title = node.xpath("./text()").to_s
-			puts attrib[i].to_s + "\t: " + title
-			i += 1
+			values.push(node.xpath("./text()").to_s)
 		end
-		
+
 		# 講義題目, 担当教員
-		puts "-----\n"
-		attrib = ["講義題目", "担当教員"]
-		i = 0
 		page.doc.xpath("/html/body//table[@class='detail']//td[@class='basic2']").each do |node|
-			title = node.xpath("./text()").to_s
-			puts attrib[i].to_s + "\t: " + title
-			i += 1
+			values.push(node.xpath("./text()").to_s)
 		end
 		
-		# テキスト
-		puts "-----\n"
+		# 教科書
 		page.doc.xpath("/html/body//table[@class='detail']//text").each do |node|
-			title = node.xpath("./text()").to_s
-			puts "教科書\t: " + title
+			values.push(node.xpath("./text()").to_s)
 		end
 		
-		# 担当教員, 所属
-		puts "-----\n"
+		# 担当教員氏名
 		page.doc.xpath("/html/body//table[@class='detail']//td[@class='kyoin1']").each do |node|
-			title = node.xpath("./text()").to_s
-			puts "担当教員\t: " + title
+			values.push(node.xpath("./text()").to_s)
 		end
+		# 担当教員所属
 		page.doc.xpath("/html/body//table[@class='detail']//td[@class='kyoin2']").each do |node|
-			title = node.xpath("./text()").to_s
-			puts "所属\t: " + title
+			values.push(node.xpath("./text()").to_s)
 		end
 	end
+end
+
+# 表示
+i = 0
+values.each do |val|
+	puts attributes[i] + "\t: " + val
+	i += 1
 end
